d034f23433a7c6a352430c55a538c38ae537b2a8
asmita2206 <asmitagavhane06@gmail.com>
2020-12-22 14:54:34 +0530

    commit for Inquiry

---
+++ src/main/java/com/bluescript/inquirydetails/InquirydetailsApplication.java
@@ -0,0 +1,13 @@
+package com.bluescript.inquirydetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquirydetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquirydetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluescript/inquirydetails/InquirydetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluescript.inquirydetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquirydetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
